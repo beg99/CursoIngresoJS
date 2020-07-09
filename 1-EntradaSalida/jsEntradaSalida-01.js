03ce05895385a1ef14a562c8d60da87fd282984a
Brian Gonzalez <gonzalezbeg99@gmail.com>
2020-07-09 00:03:44 -0300

    ricky fort

--- 1-EntradaSalida/jsEntradaSalida-01.js
+++ 1-EntradaSalida/jsEntradaSalida-01.js
@@ -1,8 +1,8 @@
 /*
 Al presionar el  botón, 
 se debe mostrar un mensaje como el siguiente "Esto funciona de maravilla"*/
-function mostrar()
+function mostrar(maiamiiii)
 {
-	alert("Villegas");
-}
+	alert("hola maiamiiii");
+	}
 
